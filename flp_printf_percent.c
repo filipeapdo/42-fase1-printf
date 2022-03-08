94796bcf2da12f0c1b36572479a71637707d43ef
fiaparec <fiaparec@student.42sp.org.br>
2022-03-08 08:02:54 -0300

    printf (Bonus): 7 out 9 (%%), not done w/o tests

--- flp_printf_percent.c
+++ flp_printf_percent.c
@@ -6,14 +6,15 @@
 /*   By: fiaparec <fiaparec@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/28 10:27:48 by fiaparec          #+#    #+#             */
-/*   Updated: 2022/03/06 17:13:54 by fiaparec         ###   ########.fr       */
+/*   Updated: 2022/03/08 07:12:28 by fiaparec         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf_bonus.h"
 
-int	flp_printf_percent(void)
+int	flp_printf_percent(t_print *tab)
 {
 	ft_putchar_fd('%', 1);
-	return (1);
+	tab->rtrn += 1;
+	return (tab->rtrn);
 }
